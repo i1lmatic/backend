c3cff092790d2ab1dcea686877dd0e46ac249ce1
i1lmatic <patrickrobinhopaniquepuma@gmail.com>
2025-08-01 19:09:58 -0500

    commit
    sadasdas

--- empresas/models.py
+++ empresas/models.py
@@ -1,45 +1,12 @@
+# models.py
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin,Group,Permission
+from django.contrib.auth.models import User
 
-class EmpresaManager(BaseUserManager):
-    def create_user(self, email, nombre_comercial, password=None, **extra_fields):
-        if not email:
-            raise ValueError('El email es obligatorio')
-        email = self.normalize_email(email)
-        empresa = self.model(email=email, nombre_comercial=nombre_comercial, **extra_fields)
-        empresa.set_password(password)
-        empresa.save(using=self._db)
-        return empresa
-
-    def create_superuser(self, email, nombre_comercial, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-        return self.create_user(email, nombre_comercial, password, **extra_fields)
-
-class Empresa(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(unique=True)
+class Empresa(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='empresa')
     nombre_comercial = models.CharField(max_length=255)
     direccion = models.TextField(blank=True)
     telefono = models.CharField(max_length=20, blank=True)
-    is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
-
-
-    
-    objects = EmpresaManager()
-
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['nombre_comercial']
-    groups = models.ManyToManyField(
-        Group,
-        related_name='empresa_set',  # 👈 nombre único
-        blank=True
-    )
-    user_permissions = models.ManyToManyField(
-        Permission,
-        related_name='empresa_permissions',
-        blank=True
-    )
 
     def __str__(self):
         return self.nombre_comercial
